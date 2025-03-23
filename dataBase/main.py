9ef932e4f7453d38e89b57325e653d1f1d1ea5cf
Fernando_DEV <fernandoperuci2004@gmail.com>
2025-03-23 15:49:44 -0300

    Ajustes no programa!

--- dataBase/main.py
+++ dataBase/main.py
@@ -8,17 +8,20 @@ db_config = {
 }
 
 def conect_db():
-    return pymysql.connect(host='localhost', user= 'ADM_FER', password= 'Venom@326$s21', database= 'corp')
+    return pymysql.connect(host= db_config['host'], user= db_config['user'], password= db_config['password'], database= db_config['database'])
 
 sql_command = input('Digite o comando SQL: ')
 
 if sql_command != '':
     connection = conect_db()
+
     with connection.cursor() as cursor:
         cursor.execute(sql_command)
         user = cursor.fetchmany(50000)
     connection.close()
-
+    
+    print('-> DB Connectet!')
     print(user) #Retorna bloco de código!
+
 else:
     print('Não conectado!')
